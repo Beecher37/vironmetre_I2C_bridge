8707b7d620c1831907ccb42a4a77bce9cd36032e
BackSnip3 <thomas.prioul@outlook.com>
2016-05-16 11:28:04 +0200

    Used dirty macros to reuse more code

--- i2c/i2c_helpers.c
+++ i2c/i2c_helpers.c
@@ -1,5 +1,4 @@
 #include "i2c_helpers.h"
-#include "../mcc_generated_files/i2c.h"
 #include "../mcc_generated_files/pin_manager.h"
 
 #define I2C_RETRY_MAX 5
@@ -19,6 +18,36 @@ void Debounce(bool rawPinValue, bool* oldStableValue, uint16_t* debounceCount) {
     }
 }
 
+bool I2C_Operation(uint8_t dev_addr, uint8_t* p_buffer, uint8_t length,
+        void (*functionPtr)(uint8_t*,uint8_t,uint16_t, I2C_MESSAGE_STATUS*))
+{
+    uint8_t timeOut = 0;
+    I2C_MESSAGE_STATUS status = I2C_MESSAGE_PENDING;
+
+    while (!I2C_MasterQueueIsEmpty());
+
+    // Write reg pointer
+    while (status != I2C_MESSAGE_FAIL) {
+        // write bytes to the device
+        (*functionPtr)(p_buffer, length, dev_addr, &status);
+
+        // wait for the message to be sent or status has changed.
+        while (status == I2C_MESSAGE_PENDING && SensorState.plugged);
+
+        if (!SensorState.plugged)
+            return false;
+
+        if (status == I2C_MESSAGE_COMPLETE)
+            return true;
+
+        // check for max retry and skip this byte
+        if (timeOut++ >= I2C_RETRY_MAX)
+            break;
+    }
+
+    return false;
+}
+
 uint8_t I2C_FirstDevice() {
     uint8_t addr = 0;
     uint8_t buffer = 0;
@@ -33,64 +62,3 @@ uint8_t I2C_FirstDevice() {
 
     return 0;
 }
-
-bool I2C_ReadBytes(uint8_t dev_addr, uint8_t* p_buffer, uint8_t length) {
-    uint8_t timeOut = 0;
-    I2C_MESSAGE_STATUS status = I2C_MESSAGE_PENDING;
-
-    while (!I2C_MasterQueueIsEmpty());
-
-    while (status != I2C_MESSAGE_FAIL) {
-        // read buffer from device
-        I2C_MasterRead(p_buffer, length, dev_addr, &status);
-
-        // wait for the message to be sent or status has changed.
-        while (status == I2C_MESSAGE_PENDING && SensorState.plugged);
-
-        if (!SensorState.plugged)
-            return false;
-
-        if (status == I2C_MESSAGE_COMPLETE)
-            return true;
-
-        // check for max retry and skip this byte
-        if (timeOut >= I2C_RETRY_MAX)
-            break;
-        else
-            timeOut++;
-    }
-
-    return false;
-}
-
-bool I2C_WriteBytes(uint8_t dev_addr, uint8_t* p_buffer, uint8_t length) {
-    uint8_t timeOut = 0;
-    I2C_MESSAGE_STATUS status = I2C_MESSAGE_PENDING;
-
-    while (!I2C_MasterQueueIsEmpty());
-
-    // Write reg pointer
-    while (status != I2C_MESSAGE_FAIL) {
-        // write bytes to the device
-        I2C_MasterWrite(p_buffer, length, dev_addr, &status);
-
-        // wait for the message to be sent or status has changed.
-        while (status == I2C_MESSAGE_PENDING && SensorState.plugged);
-
-        if (!SensorState.plugged)
-            return false;
-
-        if (status == I2C_MESSAGE_COMPLETE)
-            return true;
-
-        // check for max retry and skip this byte
-        if (timeOut >= I2C_RETRY_MAX)
-            break;
-        else
-            timeOut++;
-    }
-
-    return false;
-}
-
-
--- i2c/i2c_helpers.h
+++ i2c/i2c_helpers.h
@@ -1,7 +1,7 @@
 /* 
  * File:   
- * Author: 
- * Comments:
+ * Author: Thomas Prioul
+ * Comments: Helper functions
  * Revision history: 
  */
 
@@ -11,27 +11,30 @@
 #include <xc.h> 
 #include <stdbool.h>
 #include <stdint.h>
+#include "../mcc_generated_files/i2c.h"
 
 #ifdef	__cplusplus
 extern "C" {
 #endif 
 
-
 #define SENSOR_DEBOUNCE_COUNT   1000
 typedef struct
 {
     bool    plugged;
-   // bool    debounced;
     uint16_t debounceCount;
 } SensorStatus_t;
 
 extern SensorStatus_t SensorState;
 
-uint8_t I2C_FirstDevice();
 void Debounce(bool rawPinValue, bool* oldStableValue, uint16_t* debounceCount);
 
-bool I2C_ReadBytes(uint8_t dev_addr, uint8_t* p_buffer, uint8_t length);
-bool I2C_WriteBytes(uint8_t dev_addr, uint8_t* p_buffer, uint8_t length);
+bool I2C_Operation(uint8_t, uint8_t*, uint8_t,
+    void (*)(uint8_t*,uint8_t,uint16_t, I2C_MESSAGE_STATUS*));
+
+#define I2C_ReadBytes(dev_addr, p_buffer, length) I2C_Operation(dev_addr, p_buffer, length, I2C_MasterRead)
+#define I2C_WriteBytes(dev_addr, p_buffer, length) I2C_Operation(dev_addr, p_buffer, length, I2C_MasterWrite)
+
+uint8_t I2C_FirstDevice();
 
 #ifdef	__cplusplus
 }
